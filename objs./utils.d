d6c56b9ee82584a47eaa64ccb1295fd6292cf0b8
Ilies Fadhli <ifadhli@bess-f2r6s6.clusters.42paris.fr>
2025-04-30 00:53:42 +0200

    2e push

--- objs./utils.d
+++
@@ -1,2 +0,0 @@
-objs./utils.o: src/utils.c includes/pipex.h lib/ft_printf/ft_printf.h \
-  lib/libft/libft.h
--- objs./v2.d
+++
@@ -1,2 +0,0 @@
-objs./v2.o: src/v2.c includes/pipex.h lib/ft_printf/ft_printf.h \
-  lib/libft/libft.h
