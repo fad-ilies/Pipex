81aeef277ffa2055cb6104b124da88c98efec725
Ilies Fadhli <ifadhli@bess-f2r2s5.clusters.42paris.fr>
2025-05-05 01:24:07 +0200

    mandatory

--- objs./mid_child.d
+++
@@ -1,2 +0,0 @@
-objs./mid_child.o: src/mid_child.c includes/pipex.h \
-  lib/ft_printf/ft_printf.h lib/libft/libft.h
--- objs./v1.d
+++
@@ -1,2 +0,0 @@
-objs./v1.o: src/v1.c includes/pipex.h lib/ft_printf/ft_printf.h \
-  lib/libft/libft.h
